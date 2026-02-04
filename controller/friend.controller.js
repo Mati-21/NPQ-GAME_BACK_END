d48e5440c9163a1ce863b6259b2770b22742fec1
Mati Melkamu <matimelkamu15@gmail.com>
2026-02-04 15:36:45 +0300

    add accepting the the friend  request logic for the back end

--- controller/friend.controller.js
+++ controller/friend.controller.js
@@ -18,6 +18,7 @@ export const sendFriendRequest = async (req, res, next) => {
     const sender = await UserModel.findById(senderId);
     const receiver = await UserModel.findById(receiverId);
 
+    
     // already friends?
     if (sender.friends.includes(receiverId)) {
       return res.status(400).json({ message: "Already friends" });
@@ -26,6 +27,12 @@ export const sendFriendRequest = async (req, res, next) => {
     // already sent?
     if (sender.friendRequests.sent.includes(receiverId)) {
       return res.status(400).json({ message: "Request already sent" });
+    }
+    // already sent?
+    if (sender.friendRequests.received.includes(receiverId)) {
+      return res
+        .status(400)
+        .json({ message: "Request already sent to you by this user" });
     }
 
     sender.friendRequests.sent.push(receiverId);
@@ -45,6 +52,48 @@ export const sendFriendRequest = async (req, res, next) => {
     }
 
     res.json({ message: "Friend request sent" });
+  } catch (error) {
+    next(error);
+  }
+};
+export const acceptFriendRequest = async (req, res, next) => {
+  try {
+    const receiverId = req.userId; // the one accepting
+    const senderId = req.params.userId;
+
+    const sender = await UserModel.findById(senderId);
+    const receiver = await UserModel.findById(receiverId);
+
+    // remove from requests
+    receiver.friendRequests.received = receiver.friendRequests.received.filter(
+      (id) => id.toString() !== senderId,
+    );
+
+    sender.friendRequests.sent = sender.friendRequests.sent.filter(
+      (id) => id.toString() !== receiverId,
+    );
+
+    // add both to friends list
+    receiver.friends.push(senderId);
+    sender.friends.push(receiverId);
+
+    sender.friendRequests.sent.push(receiverId);
+    receiver.friendRequests.received.push(senderId);
+
+    await receiver.save();
+    await sender.save();
+
+    // 🔔 notify receiver
+    const receiverSocketId = onlineUsers.get(receiverId);
+
+    if (receiverSocketId) {
+      io.to(receiverSocketId).emit("friend-request", {
+        from: senderId,
+        type: "FRIEND_REQUEST",
+      });
+    }
+
+    res.json({ message: "Friend request accepted" });
   } catch (error) {
     next(error);
   }
--- route/friend.route.js
+++ route/friend.route.js
@@ -1,9 +1,13 @@
 import express from "express";
-import { sendFriendRequest } from "../controller/friend.controller.js";
+import {
+  acceptFriendRequest,
+  sendFriendRequest,
+} from "../controller/friend.controller.js";
 import { verifyToken } from "../Middleware/auth.middleware.js";
 
 const router = express.Router();
 
 router.post("/request/:userId", verifyToken, sendFriendRequest);
+router.post("/accept/:userId", verifyToken, acceptFriendRequest);
 
 export default router;
