90c15b7b48daac7a1f85536daa4030c76756b43c
Mati Melkamu <matimelkamu15@gmail.com>
2026-07-04 01:11:01 +0300

    fix registration

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -3,25 +3,29 @@ import { createUser, signInUser } from "../service/auth.service.js";
 import { createToken } from "../service/token.service.js";
 
 export const Register = async (req, res, next) => {
-  const { username, email, password, firstName, lastName } = req.body;
-  const user = await createUser(username, email, password, firstName, lastName);
+  try {
+    const { username, email, password, firstName, lastName } = req.body;
+    const user = await createUser(username, email, password, firstName, lastName);
 
-  const token = await createToken(
-    { userId: user._id },
-    process.env.TokenSecret,
-    {
-      expiresIn: "1h", // or '60s', or 60
-    },
-  );
+    const token = await createToken(
+      { userId: user._id },
+      process.env.TokenSecret,
+      {
+        expiresIn: "1h", // or '60s', or 60
+      },
+    );
 
-  res.cookie("Access_token", token, {
-    httpOnly: true,
-    secure: true,
-    sameSite: "none",
-    maxAge: 1000 * 60 * 60,
-  });
+    res.cookie("Access_token", token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+      maxAge: 1000 * 60 * 60,
+    });
 
-  res.status(201).json({ user });
+    res.status(201).json({ user });
+  } catch (error) {
+    next(error);
+  }
 };
 
 export const login = async (req, res, next) => {
